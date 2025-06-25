a93f9d15ef9f24fe6ce1174612825576c15951de
Leo Pang <leo.pang.com>
2025-06-25 16:30:52 -0700

    Add setup.py for pip install compatibility

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="daytona_sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Patched Daytona SDK",
+    author="Your Name",
+)
